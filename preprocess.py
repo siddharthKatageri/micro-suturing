baa221331cf0914eb551c5b8da752f700dc64b9e
siddharthKatageri <siddharth.katageri19@gmail.com>
2020-06-21 20:05:55 +0530

    added mode clipping

--- preprocess.py
+++ preprocess.py
@@ -5,16 +5,28 @@ from skimage import exposure
 from skimage.exposure import match_histograms
 import numpy as np
 import os
+from scipy.signal import argrelextrema
+from scipy.signal import find_peaks
 
-def histogram_equalize(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_b, img_g, img_r = cv2.split(img)
-    #histr = cv2.calcHist([img],[0],None,[256],[0,256])
-    img_r = cv2.equalizeHist(img_r)
-    img_g = cv2.equalizeHist(img_g)
-    img_b = cv2.equalizeHist(img_b)
-    new = cv2.merge((img_r, img_g, img_b))
-    return new
+
+def find_maxpeak(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    hist,bins = np.histogram(image[:,:,0].ravel(),256,[0,256])
+
+    peaks, _ = find_peaks(hist)
+    max_peak=max(hist[peaks])
+    #to find the intensity of the pixel with highest frequency
+    for i in range(len(peaks)):
+        if hist[peaks[i]]==max_peak:
+            inst=peaks[i]
+            break
+    return inst
+
+
+def plot_y_hist(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    plt.hist(image[:,:,0].ravel(),256,[0,256])
+    plt.show()
 
 def match_wrt_channel(image, reference):
     ref_ycc = cv2.cvtColor(reference, cv2.COLOR_RGB2YCrCb)
@@ -45,43 +57,55 @@ def strech_wrt_channel(image):
     new = cv2.cvtColor(new, cv2.COLOR_YCrCb2RGB)
     return new
 
-def strech(image):
-    p5 = np.percentile(image, 5)
-    p95 = np.percentile(image, 95)
-    image_rescale = exposure.rescale_intensity(image, in_range=(p5, p95), out_range=(0,255))#, out_range=(0,255)
-    image_rescale = image_rescale.astype('uint8')
-    return image_rescale
+
+def rescale(pin, a, b, c, d):
+    f = pin - c
+    s = (b-c)/(d-c)
+    pout = (f*s)+a
+    if(pout<0):
+        return 0
+    if(pout>255):
+        return 255
+    return round(pout)
 
 
-def mode_clip_wrt_channel(image):
-    image_ycc = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-    image_y, image_cr, image_cb = cv2.split(image_ycc)
-    image_y_hist = cv2.calcHist([image_y],[0],None,[256],[0,256])
+def custom_strech(img, a, b, c, d):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if(img[i][j][0]>c and img[i][j][0]<d):
+                out = rescale(img[i][j][0], a, b, c, d)
+                img[i][j][0] = out
+    new = cv2.cvtColor(img, cv2.COLOR_YCrCb2RGB)
+    return new
 
-    plt.plot(image_y_hist)
-    plt.show()
 
 
 
 reference = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/6.png")
-image = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/1.png")
+image = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/2.png")
+plot_y_hist(image)
 
 matched = match_wrt_channel(image, reference)
+plot_y_hist(matched)
+inst = find_maxpeak(matched)
 
-#direct = match_histograms(image, reference, multichannel=True)
 cv2.imshow("Source", image)
 cv2.imshow("reference", reference)
 cv2.imshow("matched", matched)
-#cv2.imshow("direct", direct)
-#cv2.waitKey(0)
 
 streched = strech_wrt_channel(matched)
+plot_y_hist(streched)
 cv2.imshow("strech",streched)
 cv2.waitKey(0)
 
-#mode_clip_wrt_channel(image)
-#mode_clip_wrt_channel(matched)
-#mode_clip_wrt_channel(streched)
+
+new = custom_strech(matched, 200, 255, inst-40, inst+40)
+plot_y_hist(new)
+
+
+cv2.imshow("new", new)
+cv2.waitKey(0)
 
 
 
