3e32e58f04dbb5f8b71ac7f8e2b66f473f7f9afe
siddharthKatageri <siddharth.katageri19@gmail.com>
2020-06-23 23:54:08 +0530

    added modeclip then hs

--- preprocess.py
+++ preprocess.py
@@ -66,7 +66,7 @@ def rescale(pin, a, b, c, d):
         return 0
     if(pout>255):
         return 255
-    return round(pout)
+    return np.round(pout)
 
 
 def custom_strech(img, a, b, c, d):
@@ -92,27 +92,56 @@ def find_width(image):
             break
 
     result=peak_widths(hist,[inst], rel_height=0.85)
-
-    print(result)
-    plt.plot(hist)
-    plt.plot(inst, hist[inst], "x")
-    plt.hlines(*result[1:], color="C3")
-    plt.show()
-    exit()
-    return result[2], result[3]
+    result = list(result)
+    result[2]=result[2]-15
+    result[3]=result[3]+10
+    #plt.plot(hist)
+    #plt.plot(inst, hist[inst], "x")
+    #plt.hlines(*result[1:], color="C3")
+    #plt.show()
+    return np.round(result[2]), np.round(result[3])
 
 
 reference = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/6.png")
-image = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/2.png")
-plot_y_hist(image)
+image = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/69.png")
 
+
+
+#mode clipping -> histogram specification
+rl, rr = find_width(reference)
+new_ref = custom_strech(reference, 200, 255, rl, rr)
+#plot_y_hist(new_ref)
+
+
+il, ir = find_width(image)
+new_image = custom_strech(image, 200, 255, il, ir)
+#plot_y_hist(new_image)
+
+
+matched = match_wrt_channel(new_image, new_ref)
+#plot_y_hist(matched)
+
+
+cv2.imshow("new_ref", new_ref)
+cv2.imshow("new_image", new_image)
+cv2.imshow("reference", reference)
+cv2.imshow("image", image)
+cv2.imshow("matched", matched)
+cv2.waitKey(0)
+
+
+
+
+
+
+#histtogram specification -> mode clipping
+'''
+plot_y_hist(image)
 matched = match_wrt_channel(image, reference)
 plot_y_hist(matched)
 inst = find_maxpeak(matched)
 
-cv2.imshow("Source", image)
-cv2.imshow("reference", reference)
-cv2.imshow("matched", matched)
+
 
 #streched = strech_wrt_channel(matched)
 #plot_y_hist(streched)
@@ -122,8 +151,12 @@ cv2.imshow("matched", matched)
 
 new = custom_strech(matched, 200, 255, inst-50, inst+40)
 plot_y_hist(new)
+cv2.imshow("Source", image)
+cv2.imshow("reference", reference)
+cv2.imshow("matched", matched)
 cv2.imshow("new", new)
 cv2.waitKey(0)
+'''
 
 
 
@@ -149,29 +182,20 @@ cv2.waitKey(0)
 
 
 
-
-
+#to apply pre-processing for all images in a folder
 '''
 reference = cv2.imread("E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures/6.png")
 folder="E:\\CVG\\MicroSuture\\knot_depth_estimation\\dataset_80_sutures"
 for filename in sorted(os.listdir(folder)):
     image = cv2.imread(os.path.join(folder,filename))
-    #reference = cv2.cvtColor(reference, cv2.COLOR_BGR2RGB)
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    #histogram equilization
-    #ref_e = histogram_equalize(reference)
-
-
-
-    #histogram matching
-    #matched = match_histograms(image, ref_e, multichannel=True)
     matched = match_wrt_channel(image, reference)
 
-    streched = strech_wrt_channel(matched)
+    inst = find_maxpeak(matched)
+    new = custom_strech(matched, 200, 255, inst-50, inst+40)
 
     extn = os.path.split(filename)[1]
-    cv2.imwrite("./output/spec+contrast/"+str(extn),streched)
+    cv2.imwrite("./output/specification/"+str(extn),matched)
+    cv2.imwrite("./output/specification+modeclip/"+str(extn),new)
 
 print("done")
 '''
